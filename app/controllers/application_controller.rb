ac28e74b441b6cf7a72e228fa271d956cba07e94
ezaki-shogo <71621496+ezaki-shogo@users.noreply.github.com>
2020-10-16 20:16:57 +0900

    rubocopの実施

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birthday, :full_first_name, :full_last_name, :half_first_name, :half_last_name])
   end
